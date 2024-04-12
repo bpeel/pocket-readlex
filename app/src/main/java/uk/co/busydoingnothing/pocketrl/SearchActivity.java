a48dd0a1e02bc09c9e0d272d0bb3bce4fb34ccd7
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-04-12 22:12:03 +0200

    SearchActivity: Add a menu to open the about dialog

--- app/src/main/java/uk/co/busydoingnothing/pocketrl/SearchActivity.java
+++ app/src/main/java/uk/co/busydoingnothing/pocketrl/SearchActivity.java
@@ -16,16 +16,24 @@
 
 package uk.co.busydoingnothing.pocketrl;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.SpannableStringBuilder;
 import android.text.TextWatcher;
+import android.text.method.LinkMovementMethod;
+import android.text.style.URLSpan;
 import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -40,6 +48,14 @@ public class SearchActivity extends AppCompatActivity
 {
     public static final String EXTRA_SEARCH_TERM =
         "uk.co.busydoingnothing.pocketrl.SearchTerm";
+    private static final String LICENSE_URL =
+        "https://www.gnu.org/licenses/gpl-3.0-standalone.html";
+    private static final String READ_LEXICON_URL =
+        "https://readlex.pythonanywhere.com/";
+    private static final String PRIVACY_POLICY_URL =
+        "https://busydoingnothing.co.uk/pocketrl/privacy-policy.html";
+
+    private static final int DIALOG_ABOUT = 0;
 
     public static final String TAG = "pocketrlsearch";
 
@@ -104,6 +120,107 @@ public class SearchActivity extends AppCompatActivity
                               0, /* flags */
                               null /* resultReceiver */);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.menu_about) {
+            showDialog(DIALOG_ABOUT);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void linkifyAboutMessage(SpannableStringBuilder string)
+    {
+        int pos = string.toString().indexOf("@VERSION@");
+
+        if (pos != -1) {
+            String packageVersion;
+
+            try {
+                PackageManager manager = getPackageManager();
+                String packageName = getPackageName();
+                PackageInfo packageInfo =
+                    manager.getPackageInfo(packageName, 0);
+
+                packageVersion = packageInfo.versionName;
+            } catch (PackageManager.NameNotFoundException e) {
+                packageVersion = "?";
+            }
+
+            string.replace(pos, pos + 9, packageVersion);
+        }
+
+        pos = string.toString ().indexOf("Click here for");
+
+        if (pos != -1) {
+            URLSpan span = new URLSpan(LICENSE_URL);
+            string.setSpan(span, pos + 6, pos + 10, 0 /* flags */);
+        }
+
+        pos = string.toString ().indexOf("Read Lexicon");
+
+        if (pos != -1) {
+            URLSpan span = new URLSpan(READ_LEXICON_URL);
+            string.setSpan(span, pos, pos + 12, 0 /* flags */);
+        }
+
+        pos = string.toString ().indexOf("Privacy policy");
+
+        if (pos != -1) {
+            URLSpan span = new URLSpan(PRIVACY_POLICY_URL);
+            string.setSpan(span, pos, pos + 14, 0 /* flags */);
+        }
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id)
+    {
+        if (id != DIALOG_ABOUT)
+            return null;
+
+        Dialog dialog;
+        Resources res = getResources();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        SpannableStringBuilder message =
+            new SpannableStringBuilder(res.getText(R.string.about_message));
+
+        linkifyAboutMessage(message);
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+        TextView tv =
+            (TextView) layoutInflater.inflate(R.layout.about_view,
+                                              null);
+        tv.setText(message);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
+
+        builder
+            .setView(tv)
+            .setCancelable(true)
+            .setNegativeButton(R.string.close,
+                               new DialogInterface.OnClickListener() {
+                                   @Override
+                                   public void onClick (DialogInterface dialog,
+                                                        int whichButton)
+                                   {
+                                   }
+                               });
+        dialog = builder.create();
+
+        return dialog;
     }
 
     @Override
