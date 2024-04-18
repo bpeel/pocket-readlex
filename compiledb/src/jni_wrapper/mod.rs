914e3de062020777b9a2343b5630d0d1cdec0606
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-04-18 16:31:40 +0200

    compiledb: Move the JNI wrapper for transliteration to a module
    This will keep the codebase tidy for when we add more JNI wrappers.

--- compiledb/src/jni_wrapper/mod.rs
+++ compiledb/src/jni_wrapper/mod.rs
@@ -14,7 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-mod bit_reader;
-mod dictionary;
 mod transliteration;
-mod jni_wrapper;
